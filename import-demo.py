d68c85dcb0dfc910cac46cf2aa7e08a32e9b3cb3
liuchao <1104892323@qq.com>
2022-07-18 16:42:38 +0800

    no.2

---
+++ import-demo.py
@@ -0,0 +1,8 @@
+import sys
+print ('命令行参数为:')
+for i in sys.argv:
+    print (i)
+print ('\n python 路径为',sys.path)
+
+from sys import argv,path  #  导入特定的成员
+print('path:',path) # 因为已经导入path成员，所以此处引用时不需要加sys.path
